01ca57ee5b3f391404472bfd6f9cec5e8cb32d5f
jainank <ankitjain5019@gmail.com>
2024-11-15 12:47:22 -0500

    Update copyPR.js

--- copyPR.js
+++ copyPR.js
@@ -1,7 +1,6 @@
 chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
     let url = tabs[0].url;
     const dataToCopy = 'Hi Team, please review my PR: ' + url
-    console.log(dataToCopy);
     navigator.permissions.query({ name: "clipboard-write" }).then((result) => {
         if (result.state == "granted" || result.state == "prompt") {
           return navigator.clipboard.writeText(dataToCopy);
